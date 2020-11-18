df47216b1367c0c1640cd777213710f25d1e23ad
Karajo Kafka <karajokafka@gmail.com>
2020-11-18 08:23:15 -0800

    docs: Add prog. details

--- Explore_bikeshare_data.r
+++ Explore_bikeshare_data.r
@@ -1,6 +1,11 @@
 ###############
-# Author
-# Description:
+#
+# Author: Karajo Kafka
+#
+# Description: Use information from csv files to analyse data
+#   and answer three questions in R programming
+#   with different charts and summaries.
+#
 ###############
 
 nyc = read.csv('new_york_city.csv')
